1f1ef68c0fa7dfbfddd3fc434d784dd59cd5b424
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-05-14 15:55:39 +0200

    End code for lesson 211 "Create Entity Managers" - adding LocalContainerEntityManagerFactoryBean beans in configuration classes CardDatabaseConfiguration, CardHolderDatabaseConfiguration and PanDatabaseConfiguration.

--- src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
@@ -1,11 +1,14 @@
 package guruspringframework.sdjpamultidb.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import guruspringframework.sdjpamultidb.domain.cardholder.CreditCardHolder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
@@ -19,9 +22,19 @@ public class CardHolderDatabaseConfiguration {
     }
 
     @Bean
-    public DataSource cardHolderDataSource(@Qualifier("cardHolderDataSourceProperties") DataSourceProperties cardHolderDataSourceProperties){
+    public DataSource cardholderDataSource(@Qualifier("cardHolderDataSourceProperties") DataSourceProperties cardHolderDataSourceProperties){
         return cardHolderDataSourceProperties.initializeDataSourceBuilder()
                 .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean cardholderEntityManagerFactory(
+            @Qualifier("cardholderDataSource") DataSource cardholderDataSource,
+            EntityManagerFactoryBuilder builder){
+        return builder.dataSource(cardholderDataSource)
+                .packages(CreditCardHolder.class)
+                .persistenceUnit("cardholder")
                 .build();
     }
 }
