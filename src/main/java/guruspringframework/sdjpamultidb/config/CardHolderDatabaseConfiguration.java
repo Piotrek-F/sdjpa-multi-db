0971fe92990f8c0d47a43d0bc313febdb9a1b693
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-05-14 13:53:38 +0200

    End code for lesson 210 "Create Data Source Connections" - adding DataSource beans in configuration classes CardDatabaseConfiguration, CardHolderDatabaseConfiguration and PanDatabaseConfiguration.

--- src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
@@ -1,17 +1,27 @@
 package guruspringframework.sdjpamultidb.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class CardHolderDatabaseConfiguration {
 
     @Bean
     @ConfigurationProperties("spring.cardholder.datasource")
-    public DataSourceProperties cardHolderDataSourceProperties() {
+    public DataSourceProperties cardHolderDataSourceProperties(){
         return new DataSourceProperties();
     }
 
+    @Bean
+    public DataSource cardHolderDataSource(@Qualifier("cardHolderDataSourceProperties") DataSourceProperties cardHolderDataSourceProperties){
+        return cardHolderDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
 }
