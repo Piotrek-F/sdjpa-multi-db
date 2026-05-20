0cdb2033ff2133f022bf8c0bb30f863ca4492e91
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-05-20 17:43:50 +0200

    End code for lesson 219 "Configure Hibernate Schema Validation" - adding Hibernate properties to LocalContainerEntityManagerFactoryBean to validate database schema. The project JDK changed from Java 25 to Java 21. The project is not starting which is expected.

--- src/main/java/guruspringframework/sdjpamultidb/config/CardDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardDatabaseConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @EnableJpaRepositories(basePackages = "guruspringframework.sdjpamultidb.repositories.creditcard",
 entityManagerFactoryRef = "cardEntityManagerFactory", transactionManagerRef = "cardTransactionManager")
@@ -38,10 +39,19 @@ public class CardDatabaseConfiguration {
     public LocalContainerEntityManagerFactoryBean cardEntityManagerFactory(
             @Qualifier("cardDataSource") DataSource cardDataSource,
             EntityManagerFactoryBuilder builder){
-        return builder.dataSource(cardDataSource)
+
+        Properties props = new Properties();
+        props.put("hibernate.hbm2ddl.auto", "validate");
+
+        LocalContainerEntityManagerFactoryBean efb =
+                builder.dataSource(cardDataSource)
                 .packages(CreditCard.class)
                 .persistenceUnit("card")
                 .build();
+
+        efb.setJpaProperties(props);
+
+        return efb;
     }
 
     @Bean
--- src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @EnableJpaRepositories(basePackages = "guruspringframework.sdjpamultidb.repositories.cardholder",
  entityManagerFactoryRef = "cardholderEntityManagerFactory", transactionManagerRef = "cardholderTransactionManager")
@@ -38,10 +39,18 @@ public class CardHolderDatabaseConfiguration {
     public LocalContainerEntityManagerFactoryBean cardholderEntityManagerFactory(
             @Qualifier("cardholderDataSource") DataSource cardholderDataSource,
             EntityManagerFactoryBuilder builder){
-        return builder.dataSource(cardholderDataSource)
+
+        Properties props = new Properties();
+        props.put("hibernate.hbm2ddl.auto", "validate");
+
+        LocalContainerEntityManagerFactoryBean efb = builder.dataSource(cardholderDataSource)
                 .packages(CreditCardHolder.class)
                 .persistenceUnit("cardholder")
                 .build();
+
+        efb.setJpaProperties(props);
+
+        return efb;
     }
 
     @Bean
